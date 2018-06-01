02810eca09da2a0f5a00f35dcc145f6de5a48d11
Luka Savovic <luka.savovic199323@gmail.com>
2018-06-01 16:04:02 +0200

    finished

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -3,17 +3,41 @@
 var app = new Vue({
     el: '#app',
     data: {
-        passwordtype: 'password'
+        passwordtype: 'password',
+        email: '',
+        password: '',
+        emailError: '',
+        passwordError: ''
     },
     computed: {},
     methods: {
         showHidePassword: function showHidePassword() {
-            console.log(app.passwordtype);
             if (app.passwordtype == 'password') {
                 app.passwordtype = 'text';
             } else {
                 app.passwordtype = 'password';
             }
+        },
+        formValidation: function formValidation(e) {
+            e.preventDefault();
+            if (app.email == '') {
+                app.emailError = "Email is required!";
+            } else if (!app.emailValidation(app.email)) {
+                app.emailError = "Email is not valid!";
+            } else {
+                app.emailError = '';
+            };
+            if (app.password == '') {
+                app.passwordError = 'Password is required!';
+            } else if (app.password.length < 5) {
+                app.passwordError = 'Password is minimum 5 characters!';
+            } else {
+                app.passwordError = '';
+            }
+        },
+        emailValidation: function emailValidation() {
+            var regularExpresion = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regularExpresion.test(app.email);
         }
     }
 });
--- scripts/domain/vuejs.js
+++ scripts/domain/vuejs.js
@@ -2,18 +2,42 @@ var app = new Vue({
     el: '#app',
     data: {
         passwordtype: 'password',
+        email: '',
+        password: '',
+        emailError: '',
+        passwordError: ''
     },
     computed:{
 
     },
     methods: {
         showHidePassword: ()=> {
-            console.log(app.passwordtype);
             if(app.passwordtype == 'password') {
                 app.passwordtype = 'text'
             } else {
                 app.passwordtype = 'password'
             }
+        },
+        formValidation: (e)=> {
+            e.preventDefault();
+            if(app.email == '') {
+                app.emailError = "Email is required!"
+            } else if (!app.emailValidation(app.email)){
+                app.emailError = "Email is not valid!"
+            } else {
+                app.emailError = ''
+            };
+            if(app.password == ''){
+                app.passwordError = 'Password is required!'
+            } else if(app.password.length < 5){
+                app.passwordError = 'Password is minimum 5 characters!'
+            }else {
+                app.passwordError = '';
+            }
+        },
+        emailValidation: ()=> {
+            var regularExpresion = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regularExpresion.test(app.email);
         }
     }
 });
